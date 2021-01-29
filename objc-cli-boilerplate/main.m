a3ac10430deb8b98900abb98887935bb7656e984
duraki <duraki@linuxmail.org>
2021-01-29 13:01:26 +0100

    Initial Commit

---
+++ objc-cli-boilerplate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objc-cli-boilerplate
+//
+//  Created by Halis Duraki on 29/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
